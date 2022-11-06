4532b44796434fb3535b92e37c9dc0722f93bc83
Juan Pablo Figueroa <juanpablodejesus_figueroa@ucol.mx>
2022-11-06 14:06:51 -0600

    n

--- Admin/php/conexion.php
+++ Admin/php/conexion.php
@@ -1,13 +1,13 @@
 <?php
-// $server = "213.190.6.1";
-// $user = "u298097659_slg";
-// $pass = "SLG.Soca_2022";
-// $db = "u298097659_slg";
+$server = "213.190.6.1";
+$user = "u298097659_slg";
+$pass = "SLG.Soca_2022";
+$db = "u298097659_slg";
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "slg";
+// $server = "localhost";
+// $user = "root";
+// $pass = "";
+// $db = "slg";
 
 
 $conexion = new mysqli($server, $user, $pass, $db);
